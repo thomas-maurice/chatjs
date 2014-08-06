8941fcc2113346e8452e81c73ba241529d23d689
thomas <tmaurice59@gmail.com>
2014-08-06 09:15:05 +0200

    fixed tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,29 +2,29 @@ module.exports = function(grunt) {
 
   // Grunt config
   grunt.initConfig({
-		copy: {
-			main: {
-				files: [
-					{expand: true, cwd: 'bower_static/bootstrap/dist', src: ['**/*.min.*', 'fonts/**'], dest: 'static/'},
-					{expand: true, cwd: 'bower_static/jquery/dist', src: ['jquery.min.js'], dest: 'static/js/'},
-				]	
-			}
-		},
-		clean: {
-			static: [
-					'bower_static',
-					'static'
-				],
-			modules: [
-					'node_modules',
-			]
-		}
-	})
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'bower_static/bootstrap/dist', src: ['**/*.min.*', 'fonts/**'], dest: 'static/'},
+          {expand: true, cwd: 'bower_static/jquery/dist', src: ['jquery.min.js'], dest: 'static/js/'},
+        ]  
+      }
+    },
+    clean: {
+      static: [
+          'bower_static',
+          'static'
+        ],
+      modules: [
+          'node_modules',
+      ]
+    }
+  })
 
-	// Loads the grunt tasks
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-clean');
+  // Loads the grunt tasks
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   
-	// Task definition
+  // Task definition
   grunt.registerTask('default', ['copy'])
 }
