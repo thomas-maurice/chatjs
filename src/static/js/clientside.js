57fc6ca8c51b35e8df04e85d37a40bed9f187225
thomas <tmaurice59@gmail.com>
2014-08-06 15:56:59 +0200

    Now the messages have a limited persistance :)

--- src/static/js/clientside.js
+++ src/static/js/clientside.js
@@ -4,26 +4,47 @@ var socket = io.connect('http://0.0.0.0:8080');
 socket.on('connected', function(message) {
   $('#connstatus').html('Connected <i style="color:green;" class="fa fa-check"></i>');
   socket.emit('nick', nickname);
-  $('#chatconsole').prepend('<p class="text-success"><i class="fa fa-arrow-right"></i> You have joined the chatroom !</p>');
+  displayMessage('<span class="text-success"><i class="fa fa-arrow-right"></i> You have joined the chatroom !</span>');
 });
 
 socket.on('nick', function(nick) {
-  $('#chatconsole').prepend('<p class="text-info"><i class="fa fa-arrow-right"></i> '+nick+" has joined the chatroom !</p>");
+  displayMessage('<span class="text-info"><i class="fa fa-arrow-right"></i> '+nick+" has joined the chatroom !</span>");
 });
 
 socket.on('message', function(message) {
   msg = JSON.parse(message);
-  $('#chatconsole').prepend('<p><span class="text-warning"><i class="fa fa-user"></i> ' + msg.nick + '</span>  <span class="text-muted"><i class="fa fa-comment"></i> ' + msg.message + '</span></p>');
+  displayMessage('<span class="text-warning"><i class="fa fa-user"></i> ' + msg.nick + '</span>  <span class="text-muted"><i class="fa fa-comment"></i> ' + msg.message + '</span>');
 });
 
 socket.on('deco', function(nick) {
-  $('#chatconsole').prepend('<p class="text-info"><i class="fa fa-arrow-left"></i> '+nick+" has left the chatroom !</p>");
+  displayMessage('<span class="text-info"><i class="fa fa-arrow-left"></i> '+nick+" has left the chatroom !</span>");
 });
 
 socket.on('disconnect', function() {
   $('#connstatus').html('Disconnected <i style="color:red;" class="fa fa-times fa-spin"></i>');
-  $('#chatconsole').prepend('<p class="text-error"><i class="fa fa-arrow-left"></i> You have been disconnected from the server !</p>');
+  displayMessage('<span class="text-error"><i class="fa fa-arrow-left"></i> You have been disconnected from the server !</span>');
 });
+
+// Lib stuff
+function displayMessage(msg) {
+  var id = makeMessageId(20);
+  var message = '<p id="'+id+'">'+msg+'</p>';
+  $('#chatconsole').prepend(message);
+  setTimeout(function() {
+      $('#'+id).fadeOut(3000, function() {$('#'+id).remove();});
+    },120000
+  );
+}
+
+function makeMessageId(idLen) {
+    var text = "";
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < idLen; i++ )
+        text += chars.charAt(Math.floor(Math.random() * chars.length));
+
+    return text;
+}
 
 // jQuery stuff
 $(document).ready(function() {
@@ -45,7 +66,7 @@ $(document).ready(function() {
     message.nick = nickname;
     
     socket.emit("message", JSON.stringify(message));
-    $('#chatconsole').prepend('<p><span class="text-warning"><i class="fa fa-user"></i> ' + nickname + '</span>  <span class="text-muted"><i class="fa fa-comment"></i> ' + message.message + '</span></p>');
+    displayMessage('<span class="text-warning"><i class="fa fa-user"></i> ' + nickname + '</span>  <span class="text-muted"><i class="fa fa-comment"></i> ' + message.message + '</span>');
 
     $('#message').val("");
   }
